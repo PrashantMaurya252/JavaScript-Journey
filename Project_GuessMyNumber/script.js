25b0353dcc0f53b22edfe51a9f44bc4630de6fa5
PrashantMaurya252 <mauryaprashant252@gmail.com>
2023-07-28 16:29:43 +0530

    completed

--- Project_GuessMyNumber/script.js
+++ Project_GuessMyNumber/script.js
@@ -1,17 +1,30 @@
-const secretNumber=Math.trunc(Math.random()*20)+1
+let secretNumber=Math.trunc(Math.random()*20)+1
 let score=20
+let highScore=0;
 
 
-document.querySelector('.user-number').textContent=secretNumber
+
+
 document.querySelector('.check-btn').addEventListener('click',function(){
     const guess=Number(document.querySelector('.guess').value)
     console.log(guess,typeof guess)
-
+     // when there is no input
     if(!guess){
         document.querySelector('.guessing-heading').textContent='🤷‍♂️ No Number'
+       
     }
+
+    // when user win the game
     else if(guess===secretNumber){
         document.querySelector('.guessing-heading').textContent='✔ Correct Number'
+        document.querySelector('body').style.backgroundColor='red'
+        document.querySelector('.user-number').style.width='150px'
+        document.querySelector('.user-number').textContent=secretNumber
+
+        if(score>highScore){
+            highScore=score
+            document.querySelector('#high-score').textContent=highScore
+        }
     }
     else if(guess>secretNumber){
         if(score>1){
@@ -37,5 +50,18 @@ document.querySelector('.check-btn').addEventListener('click',function(){
         }
 
     }
+    
+})
+let again=document.querySelector('#again-btn').addEventListener('click',function(){
+    score=20
+    secretNumber=Math.trunc(Math.random()*20)+1;
+    document.querySelector('.guess').value=''
+    document.querySelector('#score').textContent=score
+   
+    document.querySelector('.guessing-heading').textContent="Start Guessing"
+    document.querySelector('user-number').textContent= '?'
+    document.querySelector('body').style.backgroundColor='green'
+    document.querySelector('.user-number').style.width='100px'
+    
     
 })
